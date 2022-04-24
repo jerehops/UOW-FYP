7aa647c4256b2a42a3d81cb235631f1d8151a0be
Jeremy Ho <jerehops@gmail.com>
2022-04-24 18:31:41 +0800

    remove spark dependencies from flask

--- fyp-flask/app/spark.py
+++ fyp-flask/app/spark.py
@@ -1,9 +1,4 @@
 import os, sys
-from pyspark.sql import SparkSession
-from pyspark import SparkContext, SparkConf
-from pyspark_dist_explore import hist
-import matplotlib.pyplot as plt
-from pyspark.sql import functions as F
 from flask import Blueprint, render_template, request, flash, redirect, current_app
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
