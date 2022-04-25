df52a868da1bfcf7402d4e06456134718f283585
Jeremy Ho <jerehops@gmail.com>
2022-04-26 00:02:45 +0800

    update flask frontend and spark backend

--- fyp-flask/app/spark.py
+++ fyp-flask/app/spark.py
@@ -1,30 +1,88 @@
-import os, sys
+import os
+import random
+import time
+from flask import Blueprint, render_template, request, flash, redirect, current_app, url_for, jsonify
+from datetime import datetime
 from celery import Celery
-from flask import Blueprint, render_template, request, flash, redirect, current_app
-from flask_login import login_required, current_user
-from werkzeug.utils import secure_filename
-from . import db
+from elasticsearch import Elasticsearch
 
-spark = Blueprint('spark', __name__)
-simple_app = Celery('simple_worker', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+spark = Blueprint('main', __name__)
 
-@spark.route('/simple_start_task')
-def call_method():
-    spark.logger.info("Invoking Method ")
-    #                        queue name in task folder.function name
-    r = simple_app.send_task('tasks.longtime_add', kwargs={'x': 1, 'y': 2})
-    spark.logger.info(r.backend)
-    return r.id
+# Celery configuration
+celery = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
 
-@spark.route('/simple_task_status/<task_id>')
-def get_status(task_id):
-    status = simple_app.AsyncResult(task_id, app=simple_app)
-    print("Invoking Method ")
-    return "Status of the Task " + str(status.state)
+ELASTIC_PASSWORD = "Qwerty@sd123"
+
+es = Elasticsearch("https://es01:9200", ca_certs="ca.crt", basic_auth=("elastic", ELASTIC_PASSWORD))   
 
 
-@spark.route('/simple_task_result/<task_id>')
-def task_result(task_id):
-    result = simple_app.AsyncResult(task_id).result
-    return "Result of the Task " + str(result)
+@spark.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+@spark.route('/sparktask', methods=['POST'])
+def sparktask():
+    task = spark_job_task.apply_async()
+
+    if not es.indices.exists('spark-jobs'):
+        print("creating '%s' index..." % ('spark-jobs'))
+        res = es.indices.create(index='spark-jobs', body={
+            "settings" : {
+                "number_of_shards": 1,
+                "number_of_replicas": 0
+            }
+        })
+        print(res)
+
+    es.index(index='spark-jobs', doc_type='job', id=task.id, body={
+        'current': 0, 
+        'total': 100,
+        'status': 'Spark job pending..',
+        'start_time': datetime.utcnow()
+    })
+
+    return jsonify({}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
+
+
+@spark.route('/status/<task_id>')
+def taskstatus(task_id, methods=['GET']):
+    
+    task = spark_job_task.AsyncResult(task_id)
+
+    if task.state == 'FAILURE':
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    else:
+        # otherwise get the task info from ES
+        es_task_info = es.get(index='spark-jobs',doc_type='job',id=task_id)
+        response = es_task_info['_source']
+        response['state'] = task.state
+
+    return jsonify(response)
+
+
+@celery.task(bind=True)
+def spark_job_task(self):
+
+    task_id = self.request.id
+
+    master_path = 'spark://spark-master:7077'
+
+    project_dir = '/spark'
+
+    jar_path = '/spark/jars/elasticsearch-hadoop.jar'
+
+    spark_code_path =  project_dir + '/es_spark_test.py'
+
+    os.system("~/spark/bin/spark-submit --master %s --jars %s %s %s" % 
+        (master_path, jar_path, spark_code_path, task_id))
+
+    return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': 42} 
