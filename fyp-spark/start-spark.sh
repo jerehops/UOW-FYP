2ecde5fa622a3ce0b2193b519b62919c7672b066
Jeremy Ho <jerehops@gmail.com>
2022-04-30 21:57:02 +0800

    fix spark directory

--- fyp-spark/start-spark.sh
+++ fyp-spark/start-spark.sh
@@ -1,18 +1,18 @@
 #start-spark.sh
 #!/bin/bash
-. "/spark/bin/load-spark-env.sh"
+. "/opt/spark/bin/load-spark-env.sh"
 # When the spark work_load is master run class org.apache.spark.deploy.master.Master
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
 export SPARK_MASTER_HOST=`hostname`
 
-cd /spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 # When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
-cd /spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
