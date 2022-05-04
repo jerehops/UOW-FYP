92fe97b1559d7f29de2e853e922c214062168a9f
Jeremy Ho <jerehops@gmail.com>
2022-05-04 23:25:45 +0800

    fix upload path and secret key

--- fyp-flask/config.py
+++ fyp-flask/config.py
@@ -1,17 +1,17 @@
 from os import environ, path
+import os
 base_dir = path.abspath(path.dirname(__file__))
 
 class Config:
   TESTING = True
   DEBUG = True
-  SECRET_KEY = "NobodyKnowsThisShhh!!"
-  SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI') or 'sqlite:///' + path.join(base_dir, 'user.db')
+  SECRET_KEY = os.urandom(12).hex()
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(base_dir, '/database/user.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   FLASK_ADMIN_SWATCH = 'cerulean'
   TEMPLATES_AUTO_RELOAD = True
   WTF_CSRF_ENABLED = False
   MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-  DATABASE_URI = path.join(base_dir + '/app/database/user.db')
-  UPLOAD_FOLDER = path.join('opt/data/uploads/')
-  DEFAULT_FOLDER = path.join('opt/data/default/')
+  UPLOAD_FOLDER = '/opt/data/uploads/'
+  DEFAULT_FOLDER = '/opt/data/default/'
   ALLOWED_EXTENSIONS = set(['csv'])
