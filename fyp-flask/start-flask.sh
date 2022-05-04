a98d1ab3f4185d86edbb32ea3304d32044c17cbc
Jeremy Ho <jerehops@gmail.com>
2022-05-04 21:40:11 +0800

    update:

--- fyp-flask/start-flask.sh
+++ fyp-flask/start-flask.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-echo "Moving data to shared volume"
-if [ ! -f "/opt/data/default/movie"]; then
+echo "Moving data to shared volume";
+if [ ! -f "/opt/data/default/movie" ]; then
     mv movie/ /opt/data/default/
 fi
-echo "Moving data to shared volume (done)"
-echo "Starting Flask"
+echo "Moving data to shared volume (done)";
+echo "Starting Flask";
 
 flask run --host=0.0.0.0
