aa07f220dde91722e90220e571d6a4b7018e4343
Jeremy Ho <jerehops@gmail.com>
2022-05-05 15:29:00 +0800

    update flask cookie and docker compose image tag

--- fyp-flask/config.py
+++ fyp-flask/config.py
@@ -5,7 +5,7 @@ base_dir = path.abspath(path.dirname(__file__))
 class Config:
   TESTING = True
   DEBUG = True
-  SECRET_KEY = os.urandom(12).hex()
+  SECRET_KEY = os.urandom(24).hex()
   SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI') or 'sqlite:///' + path.join(base_dir, 'user.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   FLASK_ADMIN_SWATCH = 'cerulean'
@@ -16,3 +16,4 @@ class Config:
   UPLOAD_FOLDER = path.join('/opt/data/uploads/')
   DEFAULT_FOLDER = path.join('/opt/data/default/')
   ALLOWED_EXTENSIONS = set(['csv'])
+  SESSION_COOKIE_NAME = "flask-fyp"
